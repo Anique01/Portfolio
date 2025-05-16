a9d5a96d0813b0d39b725344be7aa8618b08cc6d
Anique01 <faniquemichels812@gmail.com>
2025-05-16 17:03:41 +0200

    Added styling and better folder strugture

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.querySelector(".theme-toggle-button");
   const body = document.body;
-  const logoImage = document.querySelector("img.logo"); 
+  const logoImages = document.querySelectorAll("img.logo"); 
 
-
-  const updateLogo = () => {
-    if (body.classList.contains("dark")) {
-      logoImage.src = "Light.png"; 
-      logoImage.alt = "Dark Mode Logo";
-    } else {
-      logoImage.src = "Dark.png"; 
-      logoImage.alt = "Light Mode Logo";
-    }
+  const updateLogos = () => {
+    logoImages.forEach(logo => {
+      if (body.classList.contains("dark")) {
+        logo.src = "Logo/Light.png"; 
+        logo.alt = "Dark Mode Logo";
+      } else {
+        logo.src = "Logo/Dark.png"; 
+        logo.alt = "Light Mode Logo";
+      }
+    });
   };
-
 
   toggleButton.addEventListener("click", () => {
     body.classList.toggle("dark");
@@ -23,16 +23,14 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       localStorage.setItem("theme", "light");
     }
-    updateLogo(); 
+    updateLogos(); 
   });
-
 
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "dark") {
     body.classList.add("dark");
   } else {
-
     body.classList.remove("dark"); 
   }
-  updateLogo(); 
+  updateLogos(); 
 });
