631ddd14547d6427e07d534191a7c7de96a3b889
Anique01 <faniquemichels812@gmail.com>
2025-05-16 14:54:21 +0200

    update logo change dark/light mode

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.querySelector(".theme-toggle-button");
   const body = document.body;
+  const logoImage = document.querySelector("img.logo"); 
+
+
+  const updateLogo = () => {
+    if (body.classList.contains("dark")) {
+      logoImage.src = "Light.png"; 
+      logoImage.alt = "Dark Mode Logo";
+    } else {
+      logoImage.src = "Dark.png"; 
+      logoImage.alt = "Light Mode Logo";
+    }
+  };
+
 
   toggleButton.addEventListener("click", () => {
     body.classList.toggle("dark");
@@ -10,12 +23,16 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       localStorage.setItem("theme", "light");
     }
+    updateLogo(); 
   });
+
 
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "dark") {
     body.classList.add("dark");
   } else {
-    body.classList.remove("dark");
+
+    body.classList.remove("dark"); 
   }
+  updateLogo(); 
 });
